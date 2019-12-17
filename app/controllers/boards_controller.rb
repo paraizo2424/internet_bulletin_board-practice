11aeb935800689205eae06a94a64948d7f7fa14e
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2019-12-17 23:43:58 +0900

    スレッド作成画面からもcategory_idを入力できるように変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -69,6 +69,6 @@ class BoardsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def board_params
-      params.require(:board).permit(:name)
+      params.require(:board).permit(:name, :category_id)
     end
 end
