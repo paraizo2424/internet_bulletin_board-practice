1e16618b6c075b0f510bcc8984546aa95b0009c5
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2019-12-17 23:15:04 +0900

    投稿作成画面でboard_id1mo も入力できるように変更

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -69,6 +69,6 @@ class ContentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def content_params
-      params.require(:content).permit(:post, :user_name, :email)
+      params.require(:content).permit(:post, :board_id, :user_name, :email)
     end
 end
