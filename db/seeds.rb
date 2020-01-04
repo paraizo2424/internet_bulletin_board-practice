a7eb384e1378854ea0026395276e2933677d2d07
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2020-01-04 10:30:52 +0900

    カテゴリーに値を入力するseed値を作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+# Categoryテーブル
+category_lists = { names: [ "プログラミング","Ruby","RubyonRails","React","Nuxt","野球","サッカー","ラグビー","バドミントン","沖縄" ] }
+
+category_lists[:names].each do |category_name|
+  Category.create!(name: category_name)
+end
+
