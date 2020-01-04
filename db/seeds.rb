c79afb57d733726f17e6eb9e31fbabb02546e454
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2020-01-04 15:47:20 +0900

    999件の投稿が入るseedを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,23 @@
 # coding: utf-8
 
 # Categoryテーブル
-category_lists = { names: [ "プログラミング","Ruby","RubyonRails","React","Nuxt","野球","サッカー","ラグビー","バドミントン","沖縄" ] }
+category_lists = { names: ["プログラミング","Ruby","RubyonRails","React","Nuxt","野球","サッカー","ラグビー","バドミントン","沖縄"] }
 
-category_lists[:names].each do |category_name|
-  Category.create!(name: category_name)
+unless Category.find_by(name: category_lists[:names].first)
+  category_lists[:names].each do |category_name|
+    Category.create!(name: category_name)
+  end
 end
 
+# Boardテーブル
+board = Board.new(name: "999投稿テスト", category_id: Category.first.id)
+board.save
+
+# Contentテーブル
+999.times do |i|
+  Content.create!(
+    post: "#{ i+1 }回目の投稿です( ^ω^ )",
+    user_name: "テストさん",
+    board_id: board.id
+  )
+end
