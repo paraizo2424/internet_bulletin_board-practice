36c7eaf37d46f0e0a57865aaac23480ba95d1bcf
JunNishi <e095745@ie.u-ryukyu.ac.jp>
2020-01-08 00:56:48 +0900

    topページから遷移した先のibbページを表示する

--- app/controllers/guide_controller.rb
+++ app/controllers/guide_controller.rb
@@ -1,4 +1,8 @@
 class GuideController < ApplicationController
   def top
   end
+
+  def ibb
+    @categories = Category.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'guide#top'
+  get '/ibb' => 'guide#ibb'
+  
   resources :contents
   resources :boards
   resources :categories
